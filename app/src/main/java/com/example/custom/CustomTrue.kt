4b6b79884ea3c6c2534c4362a0806e87a74a2b8f
Han-JeongHyeon <fldkej131@gmail.com>
2022-10-31 12:48:29 +0900

    custom

--- app/src/main/java/com/example/custom/CustomTrue.kt
+++ app/src/main/java/com/example/custom/CustomTrue.kt
@@ -2,197 +2,67 @@ package com.example.custom
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.widget.RatingBar
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.drawable.toDrawable
+import com.example.custom.databinding.LayoutCustomviewBinding
+import java.util.*
 import kotlin.math.log
 
 @SuppressLint("ClickableViewAccessibility")
 class CustomTrue(context: Context, attrs: AttributeSet): ConstraintLayout(context,attrs) {
+    private var binding = LayoutCustomviewBinding.inflate(LayoutInflater.from(context), this, true)
 
-    private var mNumStars = 0
-    private var mPadding = 20
-    private var mStarWidth = 0
-    private var mStarHeight = 0
-
-    private var mMinimumStars = 0f
-    private var mRating = -1f
-    private var mStepSize = 1f
-    private var mPreviousRating = 0f
-
-    private var mIsIndicator = false
-    private var mIsScrollable = true
-    private var mIsClickable = true
-    private var mClearRatingEnabled = true
-
-    private var mStartX = 0f
-    private var mStartY = 0f
-
-    private var mEmptyDrawable: Drawable? = null
-    private var mFilledDrawable: Drawable? = null
-
-//    private var rating: RatingBar
-
-    private var text1: TextView
-    private var text2: TextView
-
+    private val TAG = "CustomView"
+    
     init {
-        val v = View.inflate(context, R.layout.layout_customview, this)
-
-        text1 = v.findViewById(R.id.text1)
-        text2 = v.findViewById(R.id.text2)
-//        rating = v.findViewById(R.id.rating)
-
-
-
-        context.theme.obtainStyledAttributes(
-            attrs,
-            R.styleable.BaseRatingBarTrue,
-            0, 0).apply {
-            try {
-
-                val typeArray = context.obtainStyledAttributes(attrs, R.styleable.BaseRatingBarTrue)
-
-//                setStepSize(typeArray.getFloat(R.styleable.BaseRatingBarTrue_stepSize, mStepSize))
-//                setNumStars(typeArray.getInt(R.styleable.BaseRatingBarTrue_numStars, mNumStars))
-            } finally {
-                recycle()
-            }
-
-        }
-
-
-
-//        rating.setOnRatingBarChangeListener { ratingBar, fl, b ->
-//            Log.d("TAG", "$ratingBar $fl $b ")
-//            rating.setOnTouchListener { view, event ->
-//
-////                rating.setOnRatingBarChangeListener { ratingBar, fl, b ->
-////                    Log.d("TAG", "$ratingBar $fl $b ")
-////                }
-//
-//                val eventX = event.x
-//                val eventY = event.y
-//
-//                when (event.action) {
-//                    MotionEvent.ACTION_DOWN -> {
-//                        mStartX = eventX
-//                        mStartY = eventY
-//                        mPreviousRating = mRating
-////                        Log.d(Custom.TAG, "1")
-//                    }
-//                    MotionEvent.ACTION_MOVE -> {
-////                        handleClickEvent(eventX)
-////                        Log.d(Custom.TAG, "2")
-//                    }
-//                    MotionEvent.ACTION_UP -> {
-////                        handleClickEvent(eventX)
-////                        Log.d(Custom.TAG, "3")
-//                    }
-//                }
-//                parent.requestDisallowInterceptTouchEvent(true)
-//                true
-//            }
-//        }
-
-//        initRatingView()
-
-        val view = v.findViewById<View>(R.id.view)
-
-        view.setOnTouchListener { view, motionEvent ->
-
-            Log.d("TAG", "text1 ${view.left} ${view.right}")
-            true
-        }
-
-        text1.setOnTouchListener { view, motionEvent ->
-
-            Log.d("TAG", "text1 ${text1.left} ${text1.right}")
-            true
-        }
-        text2.setOnTouchListener { view, motionEvent ->
-
-            Log.d("TAG", "text2 ${text2.left} ${text2.right}")
-            true
-        }
+        binding.img1.setOnClickListener { backgroumd(1) }
+        binding.img2.setOnClickListener { backgroumd(2) }
+        binding.img3.setOnClickListener { backgroumd(3) }
+        binding.img4.setOnClickListener { backgroumd(4) }
+        binding.img5.setOnClickListener { backgroumd(5) }
     }
 
-    override fun setOnTouchListener(l: OnTouchListener?) {
-        Log.d("TAG", "text1 ${text1.left} ${text1.right}")
-        Log.d("TAG", "text1 ${text2.left} ${text2.right}")
+    fun reSet() {
+        binding.img1.setBackgroundResource(R.drawable.empty)
+        binding.img2.setBackgroundResource(R.drawable.empty)
+        binding.img3.setBackgroundResource(R.drawable.empty)
+        binding.img4.setBackgroundResource(R.drawable.empty)
+        binding.img5.setBackgroundResource(R.drawable.empty)
     }
 
-//    private fun handleClickEvent(eventX: Float) {
-////        Log.d(Custom.TAG, "4")
-//        for (aa in mPartialViews!!) {
-////            Log.d(Custom.TAG, "5")
-//            if (!isPositionInRatingView(eventX, aa)) {
-//                continue
-//            }
-////            val rating =
-////                if (mStepSize != 1f) {
-////                    (aa.tag as Int).toFloat()
-////                }
-////                else{
-////
-////                    }
-////            if (mPreviousRating == rating && isClearRatingEnabled()) {
-////                setRating(mMinimumStars, true)
-////            } else {
-////                setRating(rating, true)
-////            }
-//            break
-//        }
-//    }
-//
-//    private fun initRatingView() {
-//        mPartialViews = ArrayList()
-//
-//        for (i in 1..5) {
-//            val partialView = getPartialView(
-//                i,
-//                mStarWidth,
-//                mStarHeight,
-//                mPadding
-//            )
-//            addView(partialView)
-//            mPartialViews!!.add(partialView)
-//        }
-//
-//    }
-//
-//    private var mPartialViews: MutableList<PartialViewTrue>? = null
-//
-//    private fun isPositionInRatingView(eventX: Float, aa: View): Boolean {
-//        Log.d("TAG", "$eventX ${aa.left} ${aa.right}")
-//        return eventX > aa.left && eventX < aa.right
-//    }
+    var posi: Int = 0
 
-//    private fun getPartialView(
-//        partialViewId: Int, starWidth: Int, starHeight: Int
-//        , padding: Int
-//    ): PartialViewTrue {
-//        return PartialViewTrue(context, partialViewId, starWidth, starHeight, padding)
-//    }
-//
-//    private fun setStepSize(stepSize: Float) {
-//        rating.stepSize = stepSize
-//        onRefresh()
-//    }
-//
-//    private fun setNumStars(numStars: Int){
-//        rating.numStars = numStars
-//    }
+    fun backgroumd(position: Int) {
+        reSet()
+        if (position >= 1) binding.img1.setBackgroundResource(R.drawable.filled)
+        if (position >= 2) binding.img2.setBackgroundResource(R.drawable.filled)
+        if (position >= 3) binding.img3.setBackgroundResource(R.drawable.filled)
+        if (position >= 4) binding.img4.setBackgroundResource(R.drawable.filled)
+        if (position >= 5) binding.img5.setBackgroundResource(R.drawable.filled)
 
-    private fun onRefresh(){
-        invalidate()
-        requestLayout()
+        posi = if (position == posi){
+            reSet()
+            0
+        } else position
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> Log.d(TAG, "onTouchEvent:1 ")
+            MotionEvent.ACTION_MOVE -> Log.d(TAG, "onTouchEvent:2 ")
+        }
+//        binding.img1.
+        return super.onTouchEvent(event)
     }
 
 }
